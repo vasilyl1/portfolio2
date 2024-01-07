81482f65d6f598ac404ae84cbe01e9b956292b08
vasilyl1 <likhovaido@gmail.com>
2024-01-07 16:15:04 -0500

    pagenotfound is moved to components, package.json correction

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import PageNotFound from './PageNotFound';
+import PageNotFound from './components/PageNotFound';
 import './styles/reset.css';
 import './styles/Style.css';
 
