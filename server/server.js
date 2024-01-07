5c9b4b2526b8f145aba4c5dc397d32953b03998f
vasilyl1 <likhovaido@gmail.com>
2024-01-06 22:23:41 -0500

    server part of the application

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static(path.join(__dirname, '..', '/client', '/build')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
