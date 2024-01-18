281b372f0da52f3e02cbc0da62b932b5581de3f5
vasilyl1 <likhovaido@gmail.com>
2024-01-18 11:01:33 -0500

    navigation update for mobile menu in tabs function

--- client/src/components/Tabs.js
+++ client/src/components/Tabs.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Disclosure } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
   { name: 'About', href: 'about', page: 'About' },
@@ -15,30 +16,33 @@ function classNames(...classes) {
 function Tabs({ currentPage, handlePageChange }) {
 
   return (
+    <div className="min-h-full">
     <Disclosure as="nav" className="bg-gray-800">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
+            <div className="flex h-16 items-center justify-between">
+              <div className="flex items-center">
+                <div className="flex-shrink-0">
                   <img
-                    className="h-8 w-auto"
+                    className="h-8 w-8"
                     src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                     alt="Your Company"
                   />
                 </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
+                <div className="hidden md:block">
+                  <div className="ml-10 flex items-baseline space-x-4">
                     {navigation.map((item) => (
                       <a
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          (item.name === currentPage) ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium'
+                          (item.name === currentPage) 
+                            ? 'bg-gray-900 text-white font-semibold' 
+                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-sm font-semibold'
                         )}
-                        aria-current={item.current ? 'page' : undefined}
+                        aria-current={(item.name === currentPage) ? 'page' : undefined}
                         onClick={() => handlePageChange(item.page)}
                       >
                         {item.name}
@@ -47,10 +51,22 @@ function Tabs({ currentPage, handlePageChange }) {
                   </div>
                 </div>
               </div>
+              <div className="-mr-2 flex md:hidden">
+                    {/* Mobile menu button */}
+                    <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                      <span className="absolute -inset-0.5" />
+                      <span className="sr-only">Open main menu</span>
+                      {open ? (
+                        <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                      ) : (
+                        <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                      )}
+                    </Disclosure.Button>
+                  </div>
             </div>
           </div>
-          <Disclosure.Panel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
+          <Disclosure.Panel className="md:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
               {navigation.map((item) => (
                 <Disclosure.Button
                   key={item.name}
@@ -58,10 +74,10 @@ function Tabs({ currentPage, handlePageChange }) {
                   href={item.href}
                   onClick={() => handlePageChange(item.page)}
                   className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium'
+                    (item.name === currentPage) ? 'bg-gray-900 text-white font-semibold' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block rounded-md px-3 py-2 text-base font-semibold'
                   )}
-                  aria-current={item.current ? 'page' : undefined}
+                  aria-current={(item.name === currentPage) ? 'page' : undefined}
                 >
                   {item.name}
                 </Disclosure.Button>
@@ -71,6 +87,7 @@ function Tabs({ currentPage, handlePageChange }) {
         </>
       )}
     </Disclosure>
+    </div>
   )
 }
 
