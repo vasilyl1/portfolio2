ecb937cb9e65a9aa03d3c27acf4b68bc4968e085
vasilyl1 <likhovaido@gmail.com>
2024-01-25 22:12:12 -0500

    restyled resume page and added minor corrections to contact

--- client/src/components/tabs/Contact.js
+++ client/src/components/tabs/Contact.js
@@ -58,150 +58,106 @@ function Contact() {
   return (
     <form>
       <div className='mx-auto max-w-7xl px-4 py-24 sm:px-6 sm:py-32 lg:px-8'>
-      <div className="space-y-12">
-        <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">Contact Me</h2>
-          <p className="mt-1 text-sm leading-6 text-gray-600">
-            Hello {userName}! Please leave your message:
-          </p>
+        <div className="space-y-12">
+          <div className="border-b border-gray-900/10 pb-12">
+            <h2 className="text-base text-lg font-semibold leading-7 text-gray-900">Contact Me</h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              Hello {userName}! Please leave your message below:
+            </p>
 
 
-          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div className="sm:col-span-4">
-              <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                Your Name: {userName}
-              </label>
-              <div className="mt-2">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <input
-                    type="userName"
-                    name="userName"
-                    id="username"
-                    value={userName}
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-4">
+                <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                  Your Name: {userName}
+                </label>
+                <div className="mt-2">
+                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                    <input
+                      type="userName"
+                      name="Your Name"
+                      id="username"
+                      value={userName}
+                      onChange={handleInputChange}
+                      onBlur={handleMoveOut}
+                      className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                      placeholder="name"
+                    />
+                  </div>
+                </div>
+              </div>
+
+
+              <div className="sm:col-span-4">
+                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                  Your email: {email}
+                </label>
+                <div className="mt-2">
+                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      value={email}
+                      onChange={handleInputChange}
+                      onBlur={handleMoveOut}
+                      className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                      placeholder="email"
+                    />
+                  </div>
+                </div>
+              </div>
+
+
+
+              <div className="col-span-full">
+                <label htmlFor="message" className="block text-sm font-medium leading-6 text-gray-900">
+                  Your Message: {message}
+                </label>
+                <div className="mt-2">
+                  <textarea
+                    id="message"
+                    name="Your Message"
+                    value={message}
+                    rows={3}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     onChange={handleInputChange}
                     onBlur={handleMoveOut}
-                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                    placeholder="name"
+                    placeholder="Type your message here"
                   />
                 </div>
               </div>
+
+
             </div>
 
 
-            <div className="sm:col-span-4">
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Your email: {email}
-              </label>
-              <div className="mt-2">
-                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    value={email}
-                    onChange={handleInputChange}
-                    onBlur={handleMoveOut}
-                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                    placeholder="email"
-                  />
-                </div>
+
+            {resultMessage && (
+              <div>
+                <p className="mt-1 text-sm leading-6 text-gray-600">
+                  {resultMessage}
+                </p>
               </div>
-            </div>
-
-
-
-            <div className="col-span-full">
-              <label htmlFor="message" className="block text-sm font-medium leading-6 text-gray-900">
-                Your Message: {message}
-              </label>
-              <div className="mt-2">
-                <textarea
-                  id="message"
-                  name="message"
-                  value={message}
-                  rows={3}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  onChange={handleInputChange}
-                  onBlur={handleMoveOut}
-                  placeholder="Type your message here"
-                />
-              </div>
-            </div>
-
+            )}
 
           </div>
-
-
-
-          {resultMessage && (
-            <div>
-              <p className="mt-1 text-sm leading-6 text-gray-600">
-                {resultMessage}
-              </p>
-            </div>
-          )}
-
         </div>
-      </div>
 
-      <div className="mt-6 flex items-center justify-end gap-x-6">
+        <div className="mt-6 flex items-center justify-end gap-x-6">
 
-        <button
-          type="submit"
-          onClick={handleFormSubmit}
-          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Submit
-        </button>
-      </div>
+          <button
+            type="submit"
+            onClick={handleFormSubmit}
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Submit
+          </button>
+        </div>
 
 
       </div>
     </form>
-
-
-    /*
-    <div className='n2body-left-2'>
-      <h1>Contact</h1>
-     
-      <form className="form">
-      <p>Hello {userName}! Please leave your message:</p>
-        <input
-          value={userName}
-          name="userName"
-          onChange={handleInputChange}
-          onBlur={handleMoveOut}
-          type="text"
-          placeholder="name"
-        />
-        <p>Your email: {email}</p>
-        <input
-          value={email}
-          name="email"
-          onChange={handleInputChange}
-          onBlur={handleMoveOut}
-          type="email"
-          placeholder="email"
-        />
-        <p>Your message: {message}</p>
-        <input
-          value={message}
-          name="message"
-          onChange={handleInputChange}
-          onBlur={handleMoveOut}
-          type="message"
-          placeholder="Your message here"
-          className='inputMessage'
-        />
-        <button className='nav-link' type="button" onClick={handleFormSubmit}>Submit</button>
-      </form>
-      {resultMessage && (
-        <div>
-          <p className="error-text">{resultMessage}</p>
-        </div>
-      )}
-    </div>
-    */
 
 
   );
--- client/src/components/tabs/Resume.js
+++ client/src/components/tabs/Resume.js
@@ -1,7 +1,58 @@
 import React from 'react';
 
+const posts = [
+  {
+    id: 1,
+    title: 'Front-end Proficiencies',
+    description: 
+      'HTML, CSS, JavaScript, jQuery, responsive design, React, Bootstrap, Progressive Web Applications',
+  },
+
+  {
+    id: 2,
+    title: 'Back-end Proficiencies',
+    description:
+      'API, Node, Express, MySQL, Sequelize, MongoDB, Mongoose, IndexedDB, MVC, MERN, GraphQL',
+  },
+]
+
 function Resume() {
   return (
+
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Full Stack Web Developer</h2>
+          <a 
+            className="mt-2 text-lg leading-8 text-gray-600"
+            href='https://docs.google.com/document/d/1D4vjZ32bd7KvrzL8HVomzVeLZNW_UMn4SIrfw0Hkj60/edit?usp=sharing'
+          >
+            Click here to download my Resume.
+          </a>
+        </div>
+        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          {posts.map((post) => (
+            <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
+              <div className="group relative">
+                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                  <p>
+                    <span className="absolute inset-0" />
+                    {post.title}
+                  </p>
+                </h3>
+                <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
+              </div>
+            </article>
+          ))}
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+    /*
     <div className='n2body-left-2'>
     <h1>Resume</h1><br></br>
     <a href = 'https://docs.google.com/document/d/1D4vjZ32bd7KvrzL8HVomzVeLZNW_UMn4SIrfw0Hkj60/edit?usp=sharing'>Download my resume</a><br></br>
@@ -30,6 +81,7 @@ function Resume() {
     </ul>
     <br></br>
 </div>
+*/
   );
 }
 
