5f8f553c42667fe782b367974537cf9720097a53
vasilyl1 <likhovaido@gmail.com>
2024-01-31 20:07:25 -0500

    list of projects update

--- client/src/components/data/portfolio.js
+++ client/src/components/data/portfolio.js
@@ -29,30 +29,102 @@ const projects = [
     },
     {
         name: 'CMS Blog',
-        description: 'CMS-style blog site, where developers can publish their blog posts and comment on other developers posts',
+        description: 'CMS-style blog web site, where developers can publish their blog posts and comment on other developers posts',
         repo:'https://github.com/vasilyl1/CMS-blog',
         href: 'https://cms-blog-vl.herokuapp.com/',
         tech:'Handlebars / JavaScript / CSS/Node / Express / Sequelize / MySQL / Bcrypt',
         image:'./images/techblog.png',
         imageAlt:'Image of CMS blog application'
     },
+
+    {
+        name: 'Book Search Engine',
+        description:'Google Books API search engine using the MERN stack, with a React front end, MongoDB database, and Node.js/Express.js server',
+        repo: 'https://github.com/vasilyl1/book-search-engine',
+        href: 'https://google-book-search-engine.herokuapp.com/',
+        tech: 'REACT / MERN / Apollo /JavaScript / JWT / Bcrypt / GraphQL / Bootstrap',
+        image: './images/google-books-search.png',
+        imageAlt:'Image of Google Books Search Engine application'
+    },
+
+    {
+        name: 'Text Editor Progressive Web Application',
+        description:'Single page text editor app runs in the browser and meets PWA criteria. Features a number of data persistence techniques that serve as redundancy in case one of the options is not supported by the browser.',
+        repo: 'https://github.com/vasilyl1/pwa-text-editor',
+        href: 'https://t-editor.herokuapp.com/',
+        tech: 'PWA / IndexedDB / JavaScript / Babel compiler / Webpack / Service Worker / Cache API',
+        image: './images/text-editor.png',
+        imageAlt:'Image of Text Editor Progressive Web Application'
+    },
+
+    {
+        name: 'Social Network Backend API',
+        description:'API for a social network web application. Users share their thoughts, react to friends thoughts, and create a friend lists.',
+        repo: 'https://github.com/vasilyl1/social-network-api',
+        href: 'https://watch.screencastify.com/v/YTie9yZ0CA4dzEw3l2qk',
+        tech: 'MongoDB / Mongoose ODM / JavaScript / Express.js / Node.js / MVC architecture/ OpenAI seeding',
+        image: './images/social-network-API.png',
+        imageAlt:'Image of Social Network Backend API'
+    },
+
+    {
+        name: 'E-commerce Backend API',
+        description:'The project demonstrates the functionality of the back end for an e-commerce site.',
+        repo: 'https://github.com/vasilyl1/orm-be-ecommerce',
+        href: 'https://app.screencastify.com/v3/watch/fzJCImHrKPzZAdnpbbcY',
+        tech: 'MySQL / Sequelize / JavaScript / Express.js / Node.js / MVC',
+        image: './images/e-commerce.png',
+        imageAlt:'Image of E-commerce Backend API'
+    },
+
+    {
+        name: 'Content Management System - Employee Information Tracker',
+        description:`Command-line application to manage a company's employee database`,
+        repo: 'https://github.com/vasilyl1/empl-tracker-CMS',
+        href: 'https://watch.screencastify.com/v/iJ89ifvXskMoz1FT50ZU',
+        tech: 'MySQL / Inquirer / JavaScript / Node.js',
+        image: './images/CMS-inquirer.png',
+        imageAlt:'Image of Content Management System - Employee Information Tracker'
+    },
+
     {
         name: 'Work Day Scheduler',
-        description: 'Calendar application allows to save events for each hour of the day',
+        description: 'Calendar application to save events for each hour of the day',
         repo:'https://github.com/vasilyl1/work-day-scheduler/',
         href: 'https://vasilyl1.github.io/work-day-scheduler/',
-        tech:'Handlebars / JavaScript / CSS/Node / Express / Sequelize / MySQL / Bcrypt',
+        tech:'jQuery / JavaScript / CSS/ Browser Local Storage',
         image:'./images/calendar.png',
         imageAlt:'Image of Calendar application'
     },
     {
         name: 'Weather Dashboard',
-        description: 'Weather dashboard featuring dynamic HTML relies on 3rd party API OpenWeather call',
+        description: 'Weather dashboard featuring dynamic HTML. Relies on 3rd party API OpenWeather call',
         repo:'https://github.com/vasilyl1/weather-dashboard',
         href: 'https://vasilyl1.github.io/weather-dashboard',
         tech:'API / JavaScript / CSS',
         image:'./images/weather.png',
         imageAlt:'Image of weather dashboard application'
-    }
+    },
+
+    {
+        name: 'Note Taker',
+        description: 'Write and save notes in the cloud. With the same URL you can access and modify your notes from any device',
+        repo:'https://github.com/vasilyl1/note-taker',
+        href: 'https://note-taker-vl1.herokuapp.com/',
+        tech:'JavaScript / CSS / Express JS / API / FS',
+        image:'./images/my-note.png',
+        imageAlt:'Image of Note Taker application'
+    },
+
+    {
+        name: 'SVG Logo Maker',
+        description: 'Command-line application that takes user input to generate a logo and saves it as SVG file',
+        repo:'https://github.com/vasilyl1/SVG-logo-maker',
+        href: 'https://watch.screencastify.com/v/QcZ12uPnFN3L0ToljcNw',
+        tech:'JavaScript / Node.js / Inquirer / FS',
+        image:'./images/SVG-maker.png',
+        imageAlt:'Image of SVG Logo Maker application'
+    },
+
 ];
 export {projects};
